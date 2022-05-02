03e7894a915951a3701542638890028beb28168b
martin <martinth@theodo.fr>
2022-05-02 16:33:45 +0200

    Instantiate Pokemon

---
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  name: string
+  number: number
+}
+
+export const Pokemon = ({ name, number }: Props) => {
+  return (
+    <div>
+      <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + number + ".png"}></img>
+      <p>Name : {name}</p>
+      <p>Number : {number}</p>
+    </div>
+  )
+}

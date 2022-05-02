2bbcde83d306df571b556c508bb9f4e4160c0fce
martin <martinth@theodo.fr>
2022-05-02 16:47:11 +0200

    Ok

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -3,12 +3,10 @@ interface Props {
   number: number
 }
 
-export const Pokemon = ({ name, number }: Props) => {
-  return (
-    <div>
-      <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + number + ".png"}></img>
-      <p>Name : {name}</p>
-      <p>Number : {number}</p>
-    </div>
-  )
-}
+export const Pokemon = ({ name, number }: Props) => (
+  <div>
+    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`}></img>
+    <p>Name : {name}</p>
+    <p>Number : {number}</p>
+  </div>
+)

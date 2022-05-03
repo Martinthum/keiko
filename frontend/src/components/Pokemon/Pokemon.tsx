b4d19874bf9a75b3273be8a1afbd7b8b6f0bffc8
martin <martinth@theodo.fr>
2022-05-03 09:17:25 +0200

    styling

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,12 +1,21 @@
 interface Props {
   name: string
   number: number
+  height: number
+  weight: number
 }
 
-export const Pokemon = ({ name, number }: Props) => (
-  <div>
+const capitalizeFirstLetter = (s: string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+export const Pokemon = ({ name, number, height, weight }: Props) => (
+  <div className="pokemon">
+    <br></br>
+    <p>{capitalizeFirstLetter(name)}</p>
     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`}></img>
-    <p>Name : {name}</p>
-    <p>Number : {number}</p>
+    <p>Number: {number}</p>
+    <p>Height: {height} cm</p>
+    <p>Weight: {weight} kg</p>
   </div>
 )
